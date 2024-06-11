b32eca88b1a3ab5c8821201b1a391e046c83f902
Sorin Birchi <sb.birchi.sorin@gmail.com>
2024-06-11 14:58:55 +0300

    Created packaging
    Create pyptoject.toml and setup.py files for packaging
    
    Signed-off-by: Sorin Birchi <sb.birchi.sorin@gmail.com>

--- __init__.py
+++
@@ -1,3 +0,0 @@
-"""
-brian2units
-"""
--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Sorin Birchi'
 author = 'Sorin Birchi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
